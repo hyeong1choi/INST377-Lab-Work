3148c238f03250b25000fca23b24b5259c83dd82
Hyeong Choi <hchoi129@terpmail.umd.edu>
2021-10-02 23:02:28 -0400

    app.js part 2

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -58,3 +58,14 @@ document.addEventListener('DOMContentLoaded', () => {
   
     console.log(theTetrominoes[0][0])
   
+     //randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
+
+  //draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+      squares[currentPosition + index].style.backgroundColor = colors[random]
+    })
+  }
